fe7259d6db189086458539f5433c55939b104bcc
Shahir <shahir@Mac.powerhub>
2025-12-04 23:06:26 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Preparing data for regression discontinuity design'
 
 setup(
